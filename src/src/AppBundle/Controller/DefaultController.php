e8022192dd7c589c82ed4bfe966f38609f936beb
Jonty Bale <jonty.bale@dwp.gsi.gov.uk>
2016-09-07 14:44:49 +0100

    Updating default endpoint return.

--- src/src/AppBundle/Controller/DefaultController.php
+++ src/src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -14,8 +15,6 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        return new JsonResponse([ 'ok' => true ]);
     }
 }
